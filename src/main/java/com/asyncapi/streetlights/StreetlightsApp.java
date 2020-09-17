d842127d97670f1d15ee5a02453c69f4de92566c
max.weis <max.weis@openknowledge.de>
2020-09-17 14:54:17 +0200

    add security schemes

--- src/main/java/com/asyncapi/streetlights/StreetlightsApp.java
+++ src/main/java/com/asyncapi/streetlights/StreetlightsApp.java
@@ -3,6 +3,14 @@ package com.asyncapi.streetlights;
 import com.asyncapi.api.annotations.AsyncAPI;
 import com.asyncapi.api.annotations.info.Info;
 import com.asyncapi.api.annotations.info.License;
+import com.asyncapi.api.annotations.security.OAuthFlow;
+import com.asyncapi.api.annotations.security.OAuthFlows;
+import com.asyncapi.api.annotations.security.OAuthScope;
+import com.asyncapi.api.annotations.security.SecurityRequirement;
+import com.asyncapi.api.annotations.security.SecurityScheme;
+import com.asyncapi.api.annotations.security.SecuritySchemes;
+import com.asyncapi.api.annotations.security.SecuritySchemeIn;
+import com.asyncapi.api.annotations.security.SecuritySchemeType;
 import com.asyncapi.api.annotations.server.Server;
 import com.asyncapi.api.annotations.server.ServerVariable;
 import com.asyncapi.api.annotations.server.ServerVariables;
@@ -10,9 +18,6 @@ import com.asyncapi.api.annotations.server.ServerVariables;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- *
- */
 @ApplicationPath("/")
 @AsyncAPI(
     asyncapi = "2.0.0",
@@ -41,7 +46,111 @@ import javax.ws.rs.core.Application;
                     defaultValue = "1883",
                     enumeration = { "1883", "8883" }
                 )
+            ),
+            security = {
+                @SecurityRequirement(
+                    name = "apiKey"
+                ),
+                @SecurityRequirement(
+                    name = "supportedOauthFlows",
+                    values = { "streetlights:on", "streetlights:off", "streetlights:dim" }
+                ),
+                @SecurityRequirement(
+                    name = "openIdConnectWellKnown"
+                )
+            }
+        )
+    }
+)
+@SecuritySchemes(
+    values = {
+        @SecurityScheme(
+            name = "apiKey",
+            type = SecuritySchemeType.APIKEY,
+            in = SecuritySchemeIn.USER,
+            description = "Provide your API key as the user and leave the password empty."
+        ),
+        @SecurityScheme(
+            name = "supportedOauthFlows",
+            type = SecuritySchemeType.OAUTH2,
+            description = "Flows to support OAuth 2.0",
+            flows = @OAuthFlows(
+                implicit = @OAuthFlow(
+                    authorizationUrl = "https://authserver.example/auth",
+                    scopes = {
+                        @OAuthScope(
+                            name = "streetlights:on",
+                            description = "Ability to switch lights on"
+                        ),
+                        @OAuthScope(
+                            name = "streetlights:off",
+                            description = "Ability to switch lights off"
+                        ),
+                        @OAuthScope(
+                            name = "streetlights:dim",
+                            description = "Ability to dim the lights"
+                        )
+                    }
+                ),
+                password = @OAuthFlow(
+                    tokenUrl = "https://authserver.example/token",
+                    scopes = {
+                        @OAuthScope(
+                            name = "streetlights:on",
+                            description = "Ability to switch lights on"
+                        ),
+                        @OAuthScope(
+                            name = "streetlights:off",
+                            description = "Ability to switch lights off"
+                        ),
+                        @OAuthScope(
+                            name = "streetlights:dim",
+                            description = "Ability to dim the lights"
+                        )
+                    }
+                ),
+                clientCredentials = @OAuthFlow(
+                    tokenUrl = "https://authserver.example/token",
+                    scopes = {
+                        @OAuthScope(
+                            name = "streetlights:on",
+                            description = "Ability to switch lights on"
+                        ),
+                        @OAuthScope(
+                            name = "streetlights:off",
+                            description = "Ability to switch lights off"
+                        ),
+                        @OAuthScope(
+                            name = "streetlights:dim",
+                            description = "Ability to dim the lights"
+                        )
+                    }
+                ),
+                authorizationCode = @OAuthFlow(
+                    authorizationUrl = "https://authserver.example/auth",
+                    tokenUrl = "https://authserver.example/token",
+                    refreshUrl = "https://authserver.example/refresh",
+                    scopes = {
+                        @OAuthScope(
+                            name = "streetlights:on",
+                            description = "Ability to switch lights on"
+                        ),
+                        @OAuthScope(
+                            name = "streetlights:off",
+                            description = "Ability to switch lights off"
+                        ),
+                        @OAuthScope(
+                            name = "streetlights:dim",
+                            description = "Ability to dim the lights"
+                        )
+                    }
+                )
             )
+        ),
+        @SecurityScheme(
+            name = "openIdConnectWellKnown",
+            type = SecuritySchemeType.OPENIDCONNECT,
+            openIdConnectUrl = "https://authserver.example/.well-known"
         )
     }
 )
